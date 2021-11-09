acfae6ce77c4e3d6edaf09b889665903b8245d5d
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2021-11-09 21:37:40 +0530

    react design

--- react-ecommerce/src/frontend/src/App.js
+++ react-ecommerce/src/frontend/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
-import Header from "./Component/Header";
+import Home from './Home';
+
+import Footer  from './Footer';
+import Product from './Product'
+import CheckoutCart from './CheckoutCart';
+import Header from './Header';
+import {ContextProvider, cartState, reducer } from './ContextProvider'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-        </div>
+        <ContextProvider reducer={reducer} cartState={cartState}>
+            <div>
+
+                <Router>
+                    <Header></Header>
+                    <Route exact path="/product/:id">
+                        <Product></Product>
+                    </Route>
+                    <Route exact path="/checkoutcart">
+                        <CheckoutCart></CheckoutCart>
+                    </Route>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                </Router>
+                <Footer></Footer>
+            </div>
+        </ContextProvider>
     );
 }
 
